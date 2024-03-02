bb981067ccbc1ae78a548ed6d5600a142fd79feb
daria_smyr <dasha.smyr@gmail.com>
2024-03-03 00:39:20 +0300

    Added telegram send message function

--- internal/telegram.go
+++ internal/telegram.go
@@ -32,10 +32,28 @@ func StartBot() {
 func defaultHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.Message.Chat.ID,
-		Text:   "Say /hello",
+		Text:   "Bot started",
 	})
 	if err != nil {
 		log.Printf("Enable to send default message")
 		return
 	}
+}
+
+func SendMessage(chatID int64, text string) error {
+	ctx := context.Background()
+	b, err := bot.New(os.Getenv("TELEGRAM_BOT_TOKEN"))
+	if err != nil {
+		return err
+	}
+
+	_, err = b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: chatID,
+		Text:   text,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
